46c6ff530958927c3dc5cf5a0c0c4d82de433c7d
Matthew R. Mattson <matthew@themattsoncrew.com>
2016-12-03 08:17:49 -0800

    Initial Commit
    3 Nov 2016: Created basic layout.  Styles are rough and ugly, but there is something to go from.

---
+++ src/app.js
@@ -0,0 +1,8 @@
+function autorun()
+{
+
+}
+
+if (document.addEventListener) document.addEventListener("DOMContentLoaded", autorun, false);
+else if (document.attachEvent) document.attachEvent("onreadystatechange", autorun);
+else window.onload = autorun;
