a6edb13460f30e175e8db9b6176ccf4657f892d3
Matthew R. Mattson <matthew@themattsoncrew.com>
2016-12-06 07:28:44 -0800

    Add adjustTime functionality
    6 Dec 2016: Add adjustTime function.  Wire it up to mockup.  Only log out what it should do, guts of the function will come later.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
+function adjustTime(whichTime, amount) {
+  console.log('adjustTime: ' + whichTime + ' by: ' + amount);
+}
+
+
 function autorun()
 {
-
+  document.querySelector('#ctrl-break-adjust-minus').addEventListener('click', adjustTime.bind(this, 'break', -1));
+  document.querySelector('#ctrl-break-adjust-plus').addEventListener('click', adjustTime.bind(this, 'break', 1));
+  document.querySelector('#ctrl-session-adjust-minus').addEventListener('click', adjustTime.bind(this, 'session', -1));
+  document.querySelector('#ctrl-session-adjust-plus').addEventListener('click', adjustTime.bind(this, 'session', 1));
 }
 
 if (document.addEventListener) document.addEventListener("DOMContentLoaded", autorun, false);
