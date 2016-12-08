619d99951ec3fccd3df4888c985185960d04f949
Matthew R. Mattson <matthew@themattsoncrew.com>
2016-12-08 10:32:43 -0800

    Timer countsdown!
    8 Dec 2016: Got timer to count down.  Need to add the change to break timer when session timer reaches zero.  Also need to figure out how to continue a timer that is counting down after stopping it vs reseting to a new timer.  What is this user story?

--- src/app.js
+++ src/app.js
@@ -8,22 +8,70 @@ function adjustTime(timer, whichTime, amount) {
   updateDisplay(timer);
 }
 
+
 function startStopTimer(timer) {
   console.log('start/stop timer');
+
+  timer.running = !timer.running;
+
+  updateDisplay(timer);
 }
 
+
 function updateDisplay(timer) {
+  console.log('Timer object:');
   console.log(timer);
 
+  var SESSION = 'session';
+  var BREAK = 'break';
+
+  // Session & Break time displays
   document.querySelector('#ctrl-break-display').innerText = timer.break;
   document.querySelector('#ctrl-session-display').innerText = timer.session;
 
-  var timerMin = "" + timer.runningTime.min;
+  // Update time remaining state
+  if (timer.running) {
+    if (timer.endTime == null) {
+      // Start timer
+      var endTimeMilliseconds = Date.now()
+      endTimeMilliseconds += timer.remaining.min * 60 * 1000;  // add minutes
+      endTimeMilliseconds += timer.remaining.sec * 1000;  // add seconds
+      timer.endTime = new Date();
+      timer.endTime.setTime(endTimeMilliseconds);
+
+      // Set display update interval
+      timer.updateIntervalFunc = window.setInterval(updateDisplay.bind(this, timer), 100);
+    }
+    var timeRemainingSeconds = (timer.endTime - Date.now()) / 1000;
+    timer.remaining.min = Math.floor(timeRemainingSeconds / 60);
+    timer.remaining.sec = Math.floor(timeRemainingSeconds - (timer.remaining.min * 60));
+    alert('timer.remaining.sec is less than zero! timeRemainingSeconds: ' + timeRemainingSeconds);
+
+  } else if (timer.endTime !== null) {
+    // Stop timer
+
+    // Clear update display interval function
+    window.clearInterval(timer.updateIntervalFunc);
+    timer.updateIntervalFunc = null;
+
+    // Reset time remaining to session inputs
+    // This needs work: how to we continue a timer that is already going?
+    // Answer: if break or session time have change?
+    // Answer 2: this goes in a reset button?
+    timer.remaining.type = SESSION;
+    timer.remaining.min = timer.session;
+    timer.remaining.sec = 0;
+
+    timer.endTime = null;
+  }
+
+  // Time remaining display
+  var timerMin = "" + timer.remaining.min;
   var timerSec = "";
-  if (timer.runningTime.sec < 10) {
-    timerSec += "0" + timer.runningTime.sec;
+  if (timer.remaining.sec < 10) {
+    timerSec += "0" + timer.remaining.sec;
   } else {
-    timerSec += timer.runningTime.sec;
+    timerSec += timer.remaining.sec;
   }
   document.querySelector('.ctrl-timer-countdown').innerText = timerMin + ":" + timerSec;
 }
@@ -34,17 +82,27 @@ function autorun()
   var wrappedTimer = {
     break: 5,
     session:25,
-    runningTime: {
-      min: 25,
-      sec: 0
-    }
+    running: false,
+    endTime: null,
+    remaining: {
+      type: 'session',
+      min: 24,
+      sec: 5
+    },
+    updateIntervalFunc: null
   };
 
+  // Session & Break time adjustment buttons
   document.querySelector('#ctrl-break-adjust-minus').addEventListener('click', adjustTime.bind(this, wrappedTimer, 'break', -1));
   document.querySelector('#ctrl-break-adjust-plus').addEventListener('click', adjustTime.bind(this, wrappedTimer, 'break', 1));
   document.querySelector('#ctrl-session-adjust-minus').addEventListener('click', adjustTime.bind(this, wrappedTimer, 'session', -1));
   document.querySelector('#ctrl-session-adjust-plus').addEventListener('click', adjustTime.bind(this, wrappedTimer, 'session', 1));
-  document.querySelector('.ctrl-start-stop').addEventListener('click', startStopTimer.bind(this, wrappedTimer));
+
+  // Start/Stop buttons
+  var startStopElements = document.querySelectorAll('.ctrl-start-stop')
+  startStopElements.forEach(function (element) {
+    element.addEventListener('click', startStopTimer.bind(this, wrappedTimer));
+  }.bind(this));
 
   updateDisplay(wrappedTimer);
 }
