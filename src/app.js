c7a04abb083b26a9b35c49d937427ecb0dcc60ab
Matthew R. Mattson <matthew@themattsoncrew.com>
2016-12-08 08:10:36 -0800

    Remove globals, add updateDisplay
    8 Dec 2016: Remove global and any data storage in the DOM.  That's dumb (as alluded to earlier).  Figured out how to wrap the data instead.  Move updating the DOM to a single function so to eliminate each function manipulating the DOM itself.  Already the benefit is seen.  Add startStopTimer() and attach (poorly) to the DOM.  Need to use querySelectorAll and attach to each element.  startStopTimer() only logs to the console right now.

--- src/app.js
+++ src/app.js
@@ -1,21 +1,52 @@
-function adjustTime(whichTime, amount) {
-  var currentTime = parseInt(document.querySelector('#ctrl-' + whichTime + '-display').innerText, 10);
-  var newTime = currentTime + amount;
-
+function adjustTime(timer, whichTime, amount) {
+  var newTime = timer[whichTime] + amount;
   if (newTime < 1) {
     newTime = 1;
   }
+  timer[whichTime] = newTime;
 
-  document.querySelector('#ctrl-' + whichTime + '-display').innerText = newTime;
+  updateDisplay(timer);
+}
+
+function startStopTimer(timer) {
+  console.log('start/stop timer');
+}
+
+function updateDisplay(timer) {
+  console.log(timer);
+
+  document.querySelector('#ctrl-break-display').innerText = timer.break;
+  document.querySelector('#ctrl-session-display').innerText = timer.session;
+
+  var timerMin = "" + timer.runningTime.min;
+  var timerSec = "";
+  if (timer.runningTime.sec < 10) {
+    timerSec += "0" + timer.runningTime.sec;
+  } else {
+    timerSec += timer.runningTime.sec;
+  }
+  document.querySelector('.ctrl-timer-countdown').innerText = timerMin + ":" + timerSec;
 }
 
 
 function autorun()
 {
-  document.querySelector('#ctrl-break-adjust-minus').addEventListener('click', adjustTime.bind(this, 'break', -1));
-  document.querySelector('#ctrl-break-adjust-plus').addEventListener('click', adjustTime.bind(this, 'break', 1));
-  document.querySelector('#ctrl-session-adjust-minus').addEventListener('click', adjustTime.bind(this, 'session', -1));
-  document.querySelector('#ctrl-session-adjust-plus').addEventListener('click', adjustTime.bind(this, 'session', 1));
+  var wrappedTimer = {
+    break: 5,
+    session:25,
+    runningTime: {
+      min: 25,
+      sec: 0
+    }
+  };
+
+  document.querySelector('#ctrl-break-adjust-minus').addEventListener('click', adjustTime.bind(this, wrappedTimer, 'break', -1));
+  document.querySelector('#ctrl-break-adjust-plus').addEventListener('click', adjustTime.bind(this, wrappedTimer, 'break', 1));
+  document.querySelector('#ctrl-session-adjust-minus').addEventListener('click', adjustTime.bind(this, wrappedTimer, 'session', -1));
+  document.querySelector('#ctrl-session-adjust-plus').addEventListener('click', adjustTime.bind(this, wrappedTimer, 'session', 1));
+  document.querySelector('.ctrl-start-stop').addEventListener('click', startStopTimer.bind(this, wrappedTimer));
+
+  updateDisplay(wrappedTimer);
 }
 
 if (document.addEventListener) document.addEventListener("DOMContentLoaded", autorun, false);
